31914bab6e04bc555de69193c09314115e001d78
wari777 <136269714+wari777@users.noreply.github.com>
2023-07-25 00:13:05 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,17 @@
-function skillsMember() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/skills-member.html'
-    };
+function skilsmember() {
+    var member = document.getElementById("member").value;
+    if (member == "") {
+        document.getElementById("member").style.borderColor = "red";
+        document.getElementById("member").style.borderWidth = "2px";
+        document.getElementById("member").style.borderStyle = "solid";
+        document.getElementById("member").style.borderRadius = "5px";
+        document.getElementById("member").placeholder = "Please enter your member";
+        return false;
+    } else {
+        document.getElementById("member").style.borderColor = "green";
+        document.getElementById("member").style.borderWidth = "2px";
+        document.getElementById("member").style.borderStyle = "solid";
+        document.getElementById("member").style.borderRadius = "5px";
+        return true;
+    }
 }
